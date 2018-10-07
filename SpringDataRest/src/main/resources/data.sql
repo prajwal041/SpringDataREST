4d377cb4bf64a6b16f19fb3d8aabdd8b9cacd810
prajshet <prajshet@PRAJSHET-LAP.oradev.oraclecorp.com>
2018-10-07 22:19:02 +0530

    First commit

---
+++ SpringDataRest/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into alien values(101, 'Prajwal', 'Java');
+insert into alien values(102, 'Rohit', 'Linux');
+insert into alien values(103, 'Guru', 'Python');
+insert into alien values(104, 'Babu', 'Docker');
+insert into alien values(105, 'Baana', 'Kubernetes');
